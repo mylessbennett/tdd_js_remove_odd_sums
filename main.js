7ec9d7f7f886f5d3006b88a0a947897f3cdd5e67
Myles Bennett <myles.bennett@hotmail.com>
2019-04-03 15:44:59 -0400

    changed removeOddSum function so that it passed testing

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function removeOddSum() {
+function removeOddSum(sumObjList, a, b) {
+    const evenList = sumObjList.filter(num => (num[a] + num[b]) % 2 === 0);
+    return evenList
 }
 
 module.exports = removeOddSum;
